dd04ed376b841065675758b884e1eab6e5b8451e
Akash Singh Tomar <akash.tomar217@gmail.com>
2016-11-27 07:47:03 +0530

    sleepy

--- app_email/api.py
+++ app_email/api.py
@@ -2,21 +2,35 @@ import requests
 import json
 from app_email.settings import *
 def createDroplet(server_name,size='512mb',image='ubuntu-14-04-x64'):
-
 	r = requests.post("https://api.digitalocean.com/v2/droplets", headers={"Authorization": "Bearer "+access_token},data=({"name":"example2.com","region":"nyc3","size":"512mb","image":"ubuntu-14-04-x64","backups":False}))	print r.status_code
-	print r.content
+	#check if the server is ready -- to-do
+
+	#ip_address = r["ip"]
+
+	with open('performance.sh', 'w+') as f:
+		write("#!/bin/bash")
+		write("vmstat -s")
+	bashCommand = "scp preformance.sh root@"+ip_address+":/root/performance.sh"
+	subprocess.check_output(['bash','-c', bashCommand])
+	newCommand = "ssh root@"+ip+" 'chmod +x performance.sh'"
+	subprocess.check_output(['bash','-c', newCommand])
+	return r
 def deleteDroplet(id_instance):
 	requests.delete("https://api.digitalocean.com/v2/droplets/"+str(id_instance), headers={"Authorization": "Bearer "+access_token})
 	print r.status_code
 	print r.content
+	return r
 
 def getIpAddress(droplet):
 	r = requests.get("https://api.digitalocean.com/v2/droplets/"+droplet,headers={"Content-Type": "application/json","Authorization": "Bearer "+access_token})
 	print r
+	return r
 def convertSnapshotToDroplet():
-	r2= requests.post('https://api.digitalocean.com/v2/droplets', headers={'Authorization': 'Bearer e76c1e2088c706ac7e48971fd77d4636acc806c0da47895e42705ccd107f1dc'}, data={"name":"example2.com","region":"nyc1","size":"512mb","image":"21150832"})
-	print r2
-def createSnapshot():
+	r = requests.post('https://api.digitalocean.com/v2/droplets', headers={'Authorization': 'Bearer e76c1e2088c706ac7e48971fd77d4636acc806c0da47895e42705ccd107f1dc'}, data={"name":"example2.com","region":"nyc1","size":"512mb","image":"21150832"})
+	print r
+	return r
+
+# def createSnapshot():
 
 if __name__ == "__main__":
 	createDroplet('example2.com')	
--- app_email/views.py
+++ app_email/views.py
@@ -1,45 +1,64 @@
 from django.shortcuts import render
 import subprocess
 from api import *
+import threading
 # Create your views here.
+load_balancer_exists=False
+counter=0
 def measurePerformance(ip_address):
-	bashCommand = "vmstat -s"
+	bashCommand="ssh root@"+ip_address+" './performance.sh'"
 	output = subprocess.check_output(['bash','-c', bashCommand])
 	x = output.split('\n')
 	total_memory = int(x[0].split()[0])
 	used_memory = int(x[1].split()[0])
 	memory_ratio = used_memory*1.0/total_memory
-
-	bashCommand = '''#!/bin/bash
-	CPU=$(grep -c "^processor" /proc/cpuinfo)
-	LOAD_1=$(cat /proc/loadavg | awk '{print $1}')
-	LOAD_AVERAGE_1=$(($(echo ${LOAD_1} | awk '{print 100 * $1}') / ${CPU}))
-	echo $LOAD_AVERAGE_1'''
-	output = subprocess.check_output(['bash','-c', bashCommand])
-	cpu_usage = int(output.split()[0])
-
 	performance = {
-		"memory":memory_ratio,
-		"cpu":cpu_usage
+		"memory":memory_ratio
 	}
 	return performance
 
 
 
 def optimiseDroplets(list_of_droplets):
+	sum_memory = 0
+	min_stat = -1
+	min_val = 10000
 	for droplet in list_of_droplets:
 		stats = measurePerformance(droplet)
-		if stats["memory"]>=0.75 or stats["cpu"]>=75
-			#create a new droplet
+		sum_memory+=stats["memory"]
+		if min_val > stats["memory"]:
+			min_val = stats["memory"]
+			min_stat = droplet
+	if sum_memory/len(list_of_droplets)>=0.75
+		if not load_balancer_exists:
+			createLoadBalancer()
 		else:
-			pass
+			r = createDroplet("example"+counter+".com")
+			counter+=1
+		print r
+		return r
+	elif sum_memory/len(list_of_droplets)<=0.25:
+		r = deleteDroplet(min_stat)
+		print r
+		min_stat = -1
+		min_val = 10000
+		return r
 
 
+def set_interval(func,list_of_droplets ,sec):
+    def func_wrapper():
+        set_interval(func,list_of_droplets ,sec)
+        print func(list_of_droplets)
+    t = threading.Timer(sec, func_wrapper)
+    t.start()
+    return t
 
+def maintainDroplets():
+	set_interval(optimiseDroplets,list_of_droplets,30)
 
-
-
-
-
-def test(request):
+def createLoadBalancer():
+	#create a snapshot and 2 droplets.
 	pass
+
+def start_loading():
+	maintainDroplets()
